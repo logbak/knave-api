224a4bf2f623c57068bf93bb556b2877e0fd0c29
Logan <logbak@users.noreply.github.com>
2023-09-13 22:33:26 -0400

    Adding items, traits and attributes

--- Models/PlayerCharacter.cs
+++ Models/PlayerCharacter.cs
@@ -1,6 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using Knave_API.Models;
+
 namespace Models;
 
 public class PlayerCharacter
 {
+    [Range(1, 20)]
+    public int Strength { get; set; }
+    [Range(1, 20)]
+    public int Dexterity { get; set; }
+    [Range(1, 20)]
+    public int Constitution { get; set; }
+    [Range(1, 20)]
+    public int Intelligence { get; set; }
+    [Range(1, 20)]
+    public int Wisdom { get; set; }
+    [Range(1, 20)]
+    public int Charisma { get; set; }
+    public Traits Traits { get; set; }
+    [Range(1, 20)]
+    public int Alignment { get; set; }
+    public int Armor { get; set; }
+    public int HelmetAndShield { get; set; }
+    public List<string> Inventory { get; set; }
 
 }
---
+++ Models/Traits.cs
@@ -0,0 +1,19 @@
+using Knave_API.Models.Enums;
+using Models.Enums;
+
+namespace Knave_API.Models;
+
+public class Traits
+{
+    public Background Background { get; set; }
+    public Clothing Clothing { get; set; }
+    public Face Face { get; set; }
+    public Hair Hair { get; set; }
+    public Misfortune Misfortune { get; set; }
+    public Physique Physique { get; set; }
+    public Skin Skin { get; set; }
+    public Speech Speech { get; set; }
+    public Vice Vice { get; set; }
+    public Virtue Virtue { get; set; }
+
+}
